e083f2d09d93445ed220999ede24d3c7a2cd6207
somyajainn <somyajain433@gmail.com>
2024-07-21 12:39:21 +0530

    routes updates

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const Task = require("../models/Task");
+const DeletedTask = require("../models/DeletedTask.js");
 const router = express.Router();
 const {
-  getTasks,
-  getTask,
-  postTask,
-  putTask,
-  deleteTask,
-  getTasksStats,
+	getTasks,
+	getDeletedTasks,
+	getTask,
+	postTask,
+	putTask,
+	deleteTask,
+	getTasksStats,
 } = require("../controllers/taskControllers");
 const { verifyAccessToken } = require("../middlewares.js");
 
+router.delete("/:taskId", verifyAccessToken, deleteTask);
+router.get("/deleted", verifyAccessToken, getDeletedTasks);
 router.get("/", verifyAccessToken, getTasks);
 router.post("/", verifyAccessToken, postTask);
 router.get("/stats", verifyAccessToken, getTasksStats);
 router.get("/:taskId", verifyAccessToken, getTask);
 router.put("/:taskId", verifyAccessToken, putTask);
-router.delete("/:taskId", verifyAccessToken, deleteTask);
 
 module.exports = router;
